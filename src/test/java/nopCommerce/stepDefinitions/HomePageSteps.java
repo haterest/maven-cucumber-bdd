cbaa8a27ec2ca0c9b79e5a18e30af5e04d9ee0de
haterest <ha.canvan@outlook.com>
2023-12-01 19:32:57 +0700

    update page step

--- src/test/java/nopCommerce/stepDefinitions/HomePageSteps.java
+++ src/test/java/nopCommerce/stepDefinitions/HomePageSteps.java
@@ -20,4 +20,8 @@ public class HomePageSteps extends BasePage {
         Assert.assertTrue(isElementDisplayed(driver, BasePageUI.DYNAMIC_MENU_LINK_BY_NAME, nameMenuLink));
     }
 
+    public void Just_Another_TestCase(){
+        // to do
+    }
+
 }
